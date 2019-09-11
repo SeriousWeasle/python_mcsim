1cf500b4c2cb7a6653b5098c4515145b5ca429e5
SeriousWeasle <seriousweasle@gmail.com>
2019-09-12 01:10:07 +0200

    added a handler to make things easier

--- mcsim.py
+++ mcsim.py
@@ -1,6 +1,5 @@
 #import required libraries
 import plotly.io as pio
-from tqdm import tqdm
 import math
 import time
 import random
@@ -9,46 +8,17 @@ import random
 import minecraft.animals as animals
 import minecraft.base as base
 
-#setup classes
-#animals
-cow = animals.cow_base
-pig = animals.pig_base
+cowHandler = base.animalhandler(animals.cow_base, base.lootpool, 2)
+pigHandler = base.animalhandler(animals.pig_base, base.lootpool, 2)
 
-#lootpools
-cow_lootpool = base.lootpool(cow.setup_lootpool())
-pig_lootpool = base.lootpool(pig.setup_lootpool())
+for i in range(30):
+    cowHandler.breed()
+    cowHandler.kill(math.floor(cowHandler.population.size*0.10), True, 0)
+    pigHandler.breed()
+    pigHandler.kill(math.floor(pigHandler.population.size*0.10), True, 0)
 
-#populations
-cow_population = base.population(2)
-pig_population = base.population(2)
+print(cowHandler.population.size)
+print(cowHandler.lootpool.returnloot())
 
-for iteration in tqdm(range(30), desc="Simulating breeding iterations"):
-    prevsize_cows = cow_population.size
-    cow_population.breed()
-    size_diff_cows = cow_population.size - prevsize_cows
-    for c in range(size_diff_cows):
-        cow_lootpool.addloot(cow.breed())
-    to_kill_amount_cows = math.floor(cow_population.size * 0.1)
-    cow_population.kill(to_kill_amount_cows)
-    for ck in range(to_kill_amount_cows):
-        cow_lootpool.addloot(cow.kill(True, 0))
-
-    prevsize_pigs = pig_population.size
-    pig_population.breed()
-    size_diff_pigs = pig_population.size - prevsize_pigs
-    for p in range(size_diff_pigs):
-        pig_lootpool.addloot(pig.breed())
-    to_kill_amount_pigs = math.floor(pig_population.size*0.1)
-    pig_population.kill(to_kill_amount_pigs)
-    for pk in range(to_kill_amount_pigs):
-        pig_lootpool.addloot(pig.kill(True, 0))
-
-print("Cow loot:")
-print(cow_lootpool.returnloot())
-print("Cows remaining:")
-print(cow_population.size)
-
-print("Pig loot:")
-print(pig_lootpool.returnloot())
-print("Pigs remaining:")
-print(pig_population.size)
+print(pigHandler.population.size)
+print(pigHandler.lootpool.returnloot())
--- minecraft/base.py
+++ minecraft/base.py
@@ -1,4 +1,5 @@
 import math
+import minecraft.animals as animals
 
 class lootpool:
     def __init__(self, loot_dict):
@@ -17,7 +18,9 @@ class population:
         self.size = amount
     
     def breed(self):
-        self.size = self.size + math.floor(self.size/2)
+        new_animals = math.floor(self.size/2)
+        self.size = self.size + new_animals
+        return new_animals
     
     def setSize(self, newsize):
         self.size(newsize)
@@ -30,3 +33,19 @@ class population:
             prevsize = self.size
             self.size = self.size - amount
             return {"killed": amount, "remaining": self.size}
+
+class animalhandler:
+    def __init__(self, creature, lootpool, start_size):
+        self.creature = creature
+        self.lootpool = lootpool(creature.setup_lootpool())
+        self.population = population(start_size)
+    
+    def kill(self, amount, isPlayerKill, lootingLevel):
+        kill_amount = self.population.kill(amount)["killed"]
+        for kill in range(kill_amount):
+            self.lootpool.addloot(self.creature.kill(isPlayerKill, lootingLevel))
+    
+    def breed(self):
+        newcreatures = self.population.breed()
+        for c in range(newcreatures):
+            self.lootpool.addloot(self.creature.breed())
