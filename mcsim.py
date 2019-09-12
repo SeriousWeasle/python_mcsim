ab23d0a65fad9ed19b74c1d0ae6e7087af4a3bd9
SeriousWeasle <seriousweasle@gmail.com>
2019-09-12 12:02:35 +0200

    working on an ai god help us all

--- mcsim.py
+++ mcsim.py
@@ -7,36 +7,52 @@ import random
 #import custom minecraft libraries
 import minecraft.animals as animals
 import minecraft.base as base
+import minecraft.agentlib as agentlib
 
 gamerules = {
     'entityCramming': 32
 }
 
-cowHandler00percent = base.animalhandler(animals.cow_base, base.lootpool, 2, gamerules, base.pen_base, 6, 5)
-cowHandler10percent = base.animalhandler(animals.cow_base, base.lootpool, 2, gamerules, base.pen_base, 6, 5)
-cowHandler20percent = base.animalhandler(animals.cow_base, base.lootpool, 2, gamerules, base.pen_base, 6, 5)
-cowHandler30percent = base.animalhandler(animals.cow_base, base.lootpool, 2, gamerules, base.pen_base, 6, 5)
-cowHandler40percent = base.animalhandler(animals.cow_base, base.lootpool, 2, gamerules, base.pen_base, 6, 5)
-cowHandler45percent = base.animalhandler(animals.cow_base, base.lootpool, 2, gamerules, base.pen_base, 6, 5)
+# cowHandler00percent = base.animalhandler(animals.cow_base, base.lootpool, 2, gamerules, base.pen_base, 6, 5)
+# cowHandler10percent = base.animalhandler(animals.cow_base, base.lootpool, 2, gamerules, base.pen_base, 6, 5)
+# cowHandler20percent = base.animalhandler(animals.cow_base, base.lootpool, 2, gamerules, base.pen_base, 6, 5)
+# cowHandler30percent = base.animalhandler(animals.cow_base, base.lootpool, 2, gamerules, base.pen_base, 6, 5)
+# cowHandler40percent = base.animalhandler(animals.cow_base, base.lootpool, 2, gamerules, base.pen_base, 6, 5)
+# cowHandler45percent = base.animalhandler(animals.cow_base, base.lootpool, 2, gamerules, base.pen_base, 6, 5)
 
-for i in range(20):
-    cowHandler00percent.breed()
-    cowHandler00percent.killPercent(0, True, 3)
-    cowHandler10percent.breed()
-    cowHandler10percent.killPercent(10, True, 3)
-    cowHandler20percent.breed()
-    cowHandler20percent.killPercent(20, True, 3)
-    cowHandler30percent.breed()
-    cowHandler30percent.killPercent(30, True, 3)
-    cowHandler40percent.breed()
-    cowHandler40percent.killPercent(40, True, 3)
-    cowHandler45percent.breed()
-    cowHandler45percent.killPercent(45, True, 3)
+# for i in range(20):
+#     cowHandler00percent.breed()
+#     cowHandler00percent.killPercent(0, True, 3)
+#     cowHandler10percent.breed()
+#     cowHandler10percent.killPercent(10, True, 3)
+#     cowHandler20percent.breed()
+#     cowHandler20percent.killPercent(20, True, 3)
+#     cowHandler30percent.breed()
+#     cowHandler30percent.killPercent(30, True, 3)
+#     cowHandler40percent.breed()
+#     cowHandler40percent.killPercent(40, True, 3)
+#     cowHandler45percent.breed()
+#     cowHandler45percent.killPercent(45, True, 3)
 
+# print("00%: ", cowHandler00percent.lootpool.returnloot())
+# print("10%: ", cowHandler10percent.lootpool.returnloot())
+# print("20%: ", cowHandler20percent.lootpool.returnloot())
+# print("30%: ", cowHandler30percent.lootpool.returnloot())
+# print("40%: ", cowHandler40percent.lootpool.returnloot())
+# print("45%: ", cowHandler45percent.lootpool.returnloot())
 
-print("00%: ", cowHandler00percent.lootpool.returnloot())
-print("10%: ", cowHandler10percent.lootpool.returnloot())
-print("20%: ", cowHandler20percent.lootpool.returnloot())
-print("30%: ", cowHandler30percent.lootpool.returnloot())
-print("40%: ", cowHandler40percent.lootpool.returnloot())
-print("45%: ", cowHandler45percent.lootpool.returnloot())
+#set up agents and creaturehandlers
+chandlers = {}
+agents = {}
+
+#make 100
+for i in range(100):
+    ident = 'a' + str(i)
+    agents[ident] = agentlib.agent_test(ident)
+    chandlers[ident] = base.animalhandler(animals.cow_base, base.lootpool, 2, gamerules, base.pen_base, 6, 6)
+
+agenthandler = agentlib.agent_handler(agents, chandlers)
+
+agenthandler.doIter(1)
+
+print(agenthandler.fitnesses)
---
+++ minecraft/agentlib.py
@@ -0,0 +1,78 @@
+import random
+
+class agent_test:
+    def __init__(self, identifier):
+        self.identifier = identifier
+
+        #inputs to nodes
+        self.i1_to_n1 = random.random()
+        self.i1_to_n2 = random.random()
+        self.i1_to_n3 = random.random()
+        self.i2_to_n1 = random.random()
+        self.i2_to_n2 = random.random()
+        self.i2_to_n3 = random.random()
+
+        #nodes to outputs
+        self.n1_to_o1 = random.random()
+        self.n1_to_o2 = random.random()
+        self.n2_to_o1 = random.random()
+        self.n2_to_o2 = random.random()
+        self.n3_to_o1 = random.random()
+        self.n3_to_o2 = random.random()
+
+        #output factors
+        self.killmult = random.random()
+        self.breedthreshold = random.randint(0,1000)
+
+    def randomize(self):
+        #inputs to nodes
+        self.i1_to_n1 = random.random()
+        self.i1_to_n2 = random.random()
+        self.i1_to_n3 = random.random()
+        self.i2_to_n1 = random.random()
+        self.i2_to_n2 = random.random()
+        self.i2_to_n3 = random.random()
+
+        #nodes to outputs
+        self.n1_to_o1 = random.random()
+        self.n1_to_o2 = random.random()
+        self.n2_to_o1 = random.random()
+        self.n2_to_o2 = random.random()
+        self.n3_to_o1 = random.random()
+        self.n3_to_o2 = random.random()
+
+        #output factors
+        self.killmult = random.random()
+        self.breedthreshold = random.randint(0,1000)
+    
+    def in_to_out(self, i1, i2):
+        n1 = self.i1_to_n1 * i1 + self.i2_to_n1 * i2
+        n2 = self.i1_to_n2 * i1 + self.i2_to_n2 * i2
+        n3 = self.i1_to_n3 * i1 + self.i2_to_n3 * i2
+        
+        o1_calc = self.n1_to_o1 * n1 + self.n2_to_o1 * n2 + self.n3_to_o1 * n3
+        o2 = int((self.n1_to_o2 * n1 + self.n2_to_o2 * n2 + self.n3_to_o2 * n3)*self.killmult)
+
+        if(o1_calc > self.breedthreshold):
+            o1 = True
+        else:
+            o1 = False
+        return [o1, o2]
+
+class agent_handler:
+    def __init__(self, agents, creaturehandlers):
+        self.agents = agents
+        self.creaturehandlers = creaturehandlers
+        self.fitnesses = {}
+
+    def doIter(self, num):
+        for a in self.agents:
+            ch = self.creaturehandlers[a]
+            actions = self.agents[a].in_to_out(self.creaturehandlers[a].population.size, num)
+            if actions[0]:
+                self.creaturehandlers[a].breed()
+            else:
+                self.creaturehandlers[a].kill(actions[1], True, 0)
+            amountedloot = self.creaturehandlers[a].lootpool.returnloot()
+            fitness = self.creaturehandlers[a].population.size + num*0.3 + amountedloot["leather"]*0.2 + amountedloot["beef"]*0.15 + amountedloot["experience"] * 0.25
+            self.fitnesses[a] = round(fitness,3)
